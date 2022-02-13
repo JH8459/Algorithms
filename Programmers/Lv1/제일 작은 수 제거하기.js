5a6e7612d14987648c7add4294a8b1d7d7233b28
JH <kk_ong2233@naver.com>
2022-02-14 01:04:47 +0900

    Edit: Programmers Lv1 제일 작은 수 제거하기

--- Programmers/Lv1/제일 작은 수 제거하기.js
+++ Programmers/Lv1/제일 작은 수 제거하기.js
@@ -9,6 +9,6 @@ function solution(arr) {
     var answer = [];    
     // arr 배열의 최소값보다 큰 값들만 추려서 answer 배열에 담는다
     answer = arr.filter(el => el > Math.min(...arr));
-    // arr의 배열의 길이가 1보다 같거나 작다면 [-1]을 리턴, 아닐 경우엔 answer 배열을 리턴한다
+    // arr의 배열이 빈배열이라면 [-1]을 리턴, 아닐 경우엔 answer 배열을 리턴한다
     return arr.length > 1 ? answer : [-1];
 }
