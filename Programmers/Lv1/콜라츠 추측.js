84828cca2f1741feb36d77021d7fb4087b5bb505
JH <kk_ong2233@naver.com>
2022-02-13 22:13:05 +0900

    Edit: Programmers Lv1 콜라츠 추측

---
+++ Programmers/Lv1/콜라츠 추측.js
@@ -0,0 +1,22 @@
+/*
+  문제 : Level1 - K번째수
+  문제출처 : https://programmers.co.kr/learn/courses/30/lessons/42748
+*/
+
+/*---------------------------------------*/
+// [Solution 1.]
+function solution(num) {
+    var answer = 0;
+    
+    /* num이 1이 되면 반복문 종료 */
+    while(num !== 1){
+        /* num이 짝수면 2로 나누고 홀수면 3을 곱하고 1을 더한다 */
+        if(num % 2 === 0) num /= 2;
+        else num = (num * 3) + 1;
+        // answer값 1증가
+        answer++;
+        // answer가 500보다 크면 -1을 반환하고 반복문 종료
+        if(answer >= 500) return answer = -1;
+    }
+    return answer;
+}
