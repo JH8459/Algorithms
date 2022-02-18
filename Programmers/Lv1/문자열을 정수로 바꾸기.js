c01bbda2c34aad1156562487875f7ac32d9d8ce9
JH <kk_ong2233@naver.com>
2022-02-19 01:24:33 +0900

    Edit: Programmers Lv1 문자열을 정수로 바꾸기

---
+++ Programmers/Lv1/문자열을 정수로 바꾸기.js
@@ -0,0 +1,13 @@
+/*
+  문제 : Level1 - 문자열을 정수로 바꾸기
+  문제출처 : https://programmers.co.kr/learn/courses/30/lessons/12925
+*/
+
+/*---------------------------------------*/
+// [Solution 1.]
+function solution(s) {
+    var answer = 0;
+    // 문자열 s를 정수로 변환한다.
+    answer = Number(s)
+    return answer;
+}
