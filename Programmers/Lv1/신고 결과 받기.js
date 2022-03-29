f396f18b5c919403c08562293a53ec5a29a16712
JH <kk_ong2233@naver.com>
2022-03-30 00:08:47 +0900

    Edit: Programmers Lv1 신고 결과 받기

--- Programmers/Lv1/신고 결과 받기.js
+++ Programmers/Lv1/신고 결과 받기.js
@@ -4,7 +4,7 @@
 */
 
 /*---------------------------------------*/
-// [Solution 1.] => 시간 초과로 실패
+// [Solution 1.] 시간 초과로 실패
 function solution(id_list, report, k) {
     /* 정답을 담을 배열(answer), 각 id가 신고한 유저를 담을 배열(reporter), 신고당한 횟수를 셀 배열(reported) 선언 */
     var answer = Array(id_list.length).fill(0);
@@ -38,6 +38,32 @@ function solution(id_list, report, k) {
     return answer;
 }
 
-// [Solution 2.]
-
+// [Solution 2.] 중복 없는 배열 선언 로직을 제거 => 객체의 key에 담긴 value 배열에 해당 값이 있는지 검사하는 로직으로 변경
+function solution(id_list, report, k) {
+    /* id_list의 길이로 0이 채워진 배열(answer)과 유저와 신고대상 유저가 상세히 담긴 객체(detailReport) 선언 */
+    var answer = Array(id_list.length).fill(0);
+    const detailReport = {};
+    // id_list의 요소들로 detailReport 객체의 key를 넣고 value는 빈 배열로 초기화한다
+    id_list.map(el => detailReport[el] = []);
+    /* report 요소들을 순회한다 */
+    report.map(el => {
+        // 공백(' ')으로 나눠 앞 요소는 userId, 뒷 요소는 reportId로 선언한다
+        const [userId, reportId] = el.split(' ');
+        /* 중복 신고는 제외 */
+        if(!detailReport[reportId].includes(userId)) {
+            // 신고당한 유저가 key 이고, 신고한 유저들을 value 배열에 담는다
+            detailReport[reportId].push(userId);
+        }
+    });
+    /* 객체의 키값을 순회한다 */
+    for(const key in detailReport){
+        /* 객체의 key에 담긴 배열의 길이가 k 이상이라면 이용 정지다 */
+        if(detailReport[key].length >= k) {
+            // 신고를 해서 이용정지 통보 메일의 갯수를 index에 맞춰 answer 배열에 담아준다
+            detailReport[key].map(el => answer[id_list.indexOf(el)]++);
+        }
+    }
+    // answer를 반환하며 종료한다
+    return answer;
+}
 
