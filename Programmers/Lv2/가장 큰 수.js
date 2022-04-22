2e47eb95251ed10a7ce4ea3ec492a0b5dc7d982b
JH <kk_ong2233@naver.com>
2022-04-22 14:27:42 +0900

    Edit: Programmers Lv2 가장 큰 수

--- Programmers/Lv2/가장 큰 수.js
+++ Programmers/Lv2/가장 큰 수.js
@@ -14,5 +14,5 @@ function solution(numbers) {
     // 문자열을 더한값을 내림차순으로 구한다 ex) '4' + '40' = '440', '40' + '4' = '404'
     answer = strArr.sort((a,b) => (b+a) - (a+b)).join('');
     // arrSum이 0이라면 '0'을 리턴하며 아니라면 answer를 리턴한다
-    return arrSum ? '0' : answer;
+    return arrSum ? answer : '0';
 }
